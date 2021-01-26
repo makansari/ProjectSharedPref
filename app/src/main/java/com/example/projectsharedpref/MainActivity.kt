416880aefc649291d8e26e85bdbc035537d079ec
Admin <makansari@gmail.com>
2021-01-26 14:58:51 +0530

    SHARED pref with edittext

--- app/src/main/java/com/example/projectsharedpref/MainActivity.kt
+++ app/src/main/java/com/example/projectsharedpref/MainActivity.kt
@@ -4,22 +4,35 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CheckBox
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mycheckbox : CheckBox
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        var sharedPref = getSharedPreferences("myfilename", Context.MODE_PRIVATE)
-      editTextUsername.setText(sharedPref.getString("keyusername",""))
+        mycheckbox = findViewById(R.id.checkBox)
+        var sharedPref = getSharedPreferences("myfilename5", Context.MODE_PRIVATE)
+        editTextUsername.setText(sharedPref.getString("keyusername",""))
 
 
         buttonLogin.setOnClickListener {
-               var username = editTextUsername.text.toString()
-                var editor : SharedPreferences.Editor = sharedPref.edit()
+            var username = editTextUsername.text.toString()
+            var editor : SharedPreferences.Editor = sharedPref.edit()
+
+            if(mycheckbox.isChecked){
                 editor.putString("keyusername",username)
                 editor.commit()
             }
+            else
+            {
+                editor.remove("keyusername").commit()
+            }
+
+            editTextUsername.setText("")
+
+        }
     }
 }
